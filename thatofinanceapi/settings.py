efb6716e01f439313e6da34660d3fdfc937bb2c2
David Makinde <58576209+daevid-thegreat@users.noreply.github.com>
2023-07-24 15:20:23 +0100

    formmmmm

--- thatofinanceapi/settings.py
+++ thatofinanceapi/settings.py
@@ -32,12 +32,14 @@ INSTALLED_APPS = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "https://thato.vercel.com"
+    "https://thato.vercel.com",
+    "127.0.0.1:3000",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware'
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
